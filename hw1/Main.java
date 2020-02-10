344d502047c304e9b53bc3c4b7549df5a7a429a1
Aakef Waris <awaris@hawk.iit.edu>
2020-02-09 19:42:33 -0600

    changed class name

---
+++ hw1/Main.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main{
+
+    public static void main(String[]args){
+        System.out.println("Hello World");
+    }
+
+
+    /*Insertion Sort*/
+    public ArrayList<Integer>insertionSort(ArrayList<Integer> lst){
+        return lst;
+    }
+
+    /*Merge Sort*/
+    public ArrayList<Integer> mergeSort(ArrayList<Integer> lst){
+        return lst;
+    }
+}
